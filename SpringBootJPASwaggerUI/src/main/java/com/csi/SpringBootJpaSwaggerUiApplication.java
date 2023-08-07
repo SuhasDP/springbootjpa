c159c1f7d776a86fb9d872f23fbc3a9700ca4f80
unknown <suhaspatil1798@gmail.com>
2023-08-07 13:30:15 +0530

    springboot with swagger ui

---
+++ SpringBootJPASwaggerUI/src/main/java/com/csi/SpringBootJpaSwaggerUiApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaSwaggerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaSwaggerUiApplication.class, args);
+	}
+
+}
